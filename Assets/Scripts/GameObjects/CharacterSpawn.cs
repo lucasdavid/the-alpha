cf2793b651c3e603ecacbe0a960d60eebe9f6bd5
Lucas Oliveira David <lucasolivdavid@gmail.com>
2014-02-14 16:01:13 -0500

    Invalid references of prefabs related with character spawn solved

--- Assets/Scripts/GameObjects/CharacterSpawn.cs
+++ Assets/Scripts/GameObjects/CharacterSpawn.cs
@@ -2,34 +2,36 @@
 using System.Collections;
 
 public class CharacterSpawn : MonoBehaviour {
-	public Transform nodeJawn;
-	public GameObject GenericCharacter, ScoutCharacter, TankCharacter;
+
+	public enum type {
+		generic, scout, tank
+	};
+
+	public GameObject spawnPoint;
+	public GameObject[] characters;
+
 	float cooldown = 2.0f;
 
-
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		cooldown -= Time.deltaTime;
 
 		// Figure out a cleaner way of doing this
-		if (Input.GetKeyDown(KeyCode.Q) && cooldown <= 0){
-			spawnStuff(GenericCharacter);
-		}
-		if (Input.GetKeyDown(KeyCode.W) && cooldown <= 0){
-			spawnStuff(ScoutCharacter);
-		}
-		if (Input.GetKeyDown(KeyCode.E) && cooldown <= 0){
-			spawnStuff(TankCharacter);
-		}
+		if ( Input.GetKeyDown(KeyCode.Q) && cooldown <= 0 )
+			Spawn(characters[ ( int ) type.generic] );
+		
+		if ( Input.GetKeyDown(KeyCode.W) && cooldown <= 0 )
+			Spawn(characters[ ( int ) type.scout] );
+
+		if ( Input.GetKeyDown(KeyCode.E) && cooldown <= 0 )
+			Spawn(characters[ ( int ) type.tank]);
 	}
 
-	void spawnStuff(GameObject mob) {
-        Instantiate(mob, nodeJawn.transform.position, nodeJawn.transform.rotation);
+	void Spawn(GameObject mob)
+	{
+		Debug.Log ("CharacterSpawn@Spawn");
 
+		Instantiate(mob, spawnPoint.transform.position, Quaternion.identity);
 		cooldown = 2.0f;
-		Debug.Log ("Spawn!");
 	}
 }
