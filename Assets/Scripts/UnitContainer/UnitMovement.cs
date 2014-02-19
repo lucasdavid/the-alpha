5b39d1bf365095c3b0fae7264cd940306376066e
Lucas Oliveira David <lucasolivdavid@gmail.com>
2014-02-18 21:57:22 -0500

    UnitState implemented

--- Assets/Scripts/UnitContainer/UnitMovement.cs
+++ Assets/Scripts/UnitContainer/UnitMovement.cs
@@ -3,32 +3,65 @@ using System.Collections;
 
 public class UnitMovement : MonoBehaviour {
 
+    public float AttackCooldown;
+    public bool hold;
+
 	Vector3 target;
 	bool aiming;
 	NavMeshAgent agent;
 
+    UnitState state;
+
+    float lastAttack;
+
 	void Start()
 	{
+        state = UnitState.idle;
 		agent = GetComponent<NavMeshAgent>();
+
+        lastAttack = 0;
 	}
-	
+
 	void Update ()
 	{
-		if (aiming) 
-		{
-			if ( aiming ) {
-				agent.destination = target;
-
-				// keep aiming until distance less than .1f
-				aiming = Vector3.Distance(target, transform.position) > 1.0f;
-			}
-		}
+        if ( state == UnitState.idle )
+            Idle();
+        else if ( state == UnitState.moving )
+            Moving();
+        else if ( state == UnitState.attacking )
+            Attacking();
 	}
+
+    void Idle()
+    {
+        // enemy is in range
+        // move to attacking state
+    }
+
+    void Moving ()
+    {
+        // keep aiming until distance less than .1f
+        if ( Vector3.Distance(target, transform.position) > 1.0f )
+            state = UnitState.idle;
+    }
+
+    void Attacking()
+    {
+        if ( ( lastAttack -= Time.deltaTime ) <= 0 ) {
+            lastAttack = AttackCooldown;
+
+            // attack
+        }
+    }
 
 	public void Move(Vector3 _target)
 	{
-		target = _target;
-		transform.rotation = Quaternion.LookRotation(target - transform.position);
-		aiming = true;
+        if ( hold != false )
+        {
+            transform.rotation = Quaternion.LookRotation(target - transform.position);
+
+            state = UnitState.moving;
+            agent.destination = target;
+        }
 	}
 }
