2eb6159aa848dd2193d1252a0b4c76da468c5ec1
Lucas Oliveira David <lucasolivdavid@gmail.com>
2014-03-06 12:18:45 -0500

    fix #8

---
+++ Assets/MinimapBG.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapBG : MonoBehaviour {
+
+    CameraMarquee marquee;
+
+    void Start()
+    {
+        marquee = Camera.main.GetComponent<CameraMarquee>();
+    }
+
+	void OnMouseDown()
+    {
+        marquee.mouseIsBeingUsedByHUD = true;
+    }
+
+    void OnMouseUp()
+    {
+        marquee.mouseIsBeingUsedByHUD = false;
+    }
+}
