e6de749111fa2a2c6c72d941837de153ef99255b
Lucas Oliveira David <lucasolivdavid@gmail.com>
2014-03-09 18:42:38 -0400

    Main menu implementation started

---
+++ Assets/Scripts/MainMenuContainer/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            animator.SetTrigger("exit");
+            StartCoroutine("StartGame");
+        }
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(2f);
+        Application.LoadLevelAsync("final-game-scene");
+    }
+
+}
