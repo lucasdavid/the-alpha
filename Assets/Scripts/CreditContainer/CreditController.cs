5718f26293fe543eaf32d7ee425332f7d06acf7c
Lucas Oliveira David <lucasolivdavid@gmail.com>
2014-03-11 17:38:54 -0400

    fix #20

---
+++ Assets/Scripts/CreditContainer/CreditController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditController : MonoBehaviour {
+
+    public bool skip;
+
+    public void Start()
+    {
+        StartCoroutine("CanSkip");
+    }
+
+    public void Update()
+    {
+        if (Input.anyKey || skip) {
+            Application.LoadLevel("main-menu");
+        }
+    }
+
+    IEnumerator CanSkip()
+    {
+        yield return new WaitForSeconds(40f);
+        skip = true;
+    }
+
+}
