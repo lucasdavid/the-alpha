f479da31f72ac2ec2a101eeb68078284b03c1c2f
Victor Khou <victorkhou@Victors-MacBook-Pro.local>
2014-03-11 21:03:17 -0400

    Fix #22

--- Assets/Scripts/HudContainer/Zombie.cs
+++ Assets/Scripts/HudContainer/Zombie.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class Zombie : MonoBehaviour {
+    Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
-	
+        pos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        transform.position = pos;
 	}
 }
